5cb8c1af99ba3e513fed5c671dfd7ee21a8d066b
matteocenci <matteocenci01@gmail.com>
2024-04-01 17:40:29 +0200

    feat: working in html and exercise done

--- mail/js/Mail.js
+++ mail/js/Mail.js
@@ -6,26 +6,36 @@ const mailList = [
 ];
 
 
-    const userMail = prompt("Inserisci la tua mail:");
+const sendBtnElem = document.getElementById("send-btn");
 
-    console.log(userMail);
 
+sendBtnElem.addEventListener("click", function(){
+
+    // const userMail = prompt("Inserisci la tua mail:");
+    const mailInputElem = document.getElementById("mail");
+    const mailValue = mailInputElem.value
+    
+    console.log(mailInputElem);
+    
     let found = false;
-
+    
     for (let i = 0; i < mailList.length; i++) {
         console.log(i, mailList[i]);
-        if (userMail === mailList[i]) {
+        if (mailValue === mailList[i]) {
             console.log("trovato!!!");
             found = true;
             break;
         }
     }
-
+    
     if (found === true) {
         result.innerHTML = 'accesso consentito';
     } else {
         result.innerHTML = 'accesso negato';
     }
+})
+
+
 
 // const sendBtnElem = document.getElementById("send-btn");
 // console.log(sendBtnElem);
